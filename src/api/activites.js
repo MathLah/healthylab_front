ca76807eb878cfceececf8ed0952e8e9d145fa17
Guillaume <guillaume.collet@sa-cim.fr>
2018-06-15 17:59:47 +0200

    Avancement store

---
+++ src/api/activites.js
@@ -0,0 +1,12 @@
+import axios from '../axios.config'
+
+export default {
+  async getActivites() {
+    try {
+      const reponse = await axios('/api/')
+      return reponse.data
+    } catch (error) {
+      throw (error)
+    }
+  },
+}
